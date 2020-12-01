0cc8e17a012979168359632b45867f45716cac95
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-12-01 00:54:49 +0000

    half q are done

--- cyf-ecomerce-api/server.js
+++ cyf-ecomerce-api/server.js
@@ -78,18 +78,56 @@ app.post('/customers', function (req, res){
 
 });
 
+app.put("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+  const {name,address,city,country} = req.body;
+  // get current customer values
+  pool
+    .query("SELECT * FROM customers WHERE id=$1", [customerId])
+    .then((result) => {
+        if (result.rowCount !== 1) {
+            res.status(400).send(`No customer with id ${customerId}`)
+        }
+        const customer = result.rows[0];
+        const query = 'UPDATE customers SET name=$2,address=$3,city=$4,country=$5 WHERE id=$1';
+        pool
+            .query(query, [
+                customer.id,
+                stringOrNull(name, customer.name),
+                stringOrNull(address, customer.address),
+                stringOrNull(city, customer.city),
+                stringOrNull(country, customer.country)
+            ])
+            .then((result) => {
+                if (result.rowCount !== 1) {
+                    res.status(400).send(`No customer with id ${customerId}`)
+                }
+                res.send(`Customer ${customerId} updated!`)
+            })
+            .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+
+});
+
+
 app.post('/products', function (req, res){
+  // const productId = req.body.id;
   const newProductName = req.body.product_name;
   const newUnitPrice = req.body.unit_price;
   const newSupplierId = req.body.supplier_id;
 
   pool
-  .query("SELECT * FROM products WHERE product_name=$1", [newProductName])
+  .query("SELECT * FROM products WHERE supplier_id=$1", [newSupplierId])
   .then((result) => {
-      if (result.rows.length > 0) {
+      if (result.rows.length < 0) {
         return res
           .status(400)
-          .send("A product with the same name already exists!");
+          .send(`There is no match with queried ${newSupplierId}`);
+      } else if (newUnitPrice < 0){
+        return res
+          .status(400)
+          .send(`${newUnitPrice} price must be bigger than 0`)
       } else {
         const query =
           "INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3)";
@@ -99,8 +137,35 @@ app.post('/products', function (req, res){
           .catch((e) => console.error(e));
       }
     });
-
+    
 });
+
+
+app.post('/customers/:customerId/orders', function (req, res){
+  const customerId = req.body.customer_id;
+  const newOrderDate = req.body.order_date;
+  const newOrderReference = req.body.order_reference;
+
+  pool
+  .query("SELECT * FROM orders WHERE customer_id=$1", [customerId])
+  .then((result) => {
+      if (result.rows.length < 0) {
+        return res
+          .status(400)
+          .send(`There is not any customer with that ${customerId}`);
+      } else {
+        const query =
+          "INSERT INTO orders (order_date, order_reference, customer_id) VALUES ($1, $2, $3)";
+        pool
+          .query(query, [newOrderDate, newOrderReference, customerId])
+          .then(() => res.send("Order created!"))
+          .catch((e) => console.error(e));
+      }
+    });
+
+})
+
+
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
